5a7ce40501dad34ffbbe66937bf09fed73f11720
jebcoding <johnbvillegas11@gmail.com>
2024-09-20 19:11:38 +0800

    Your commit message describing the changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+function VerseBut() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn JB
-        </a>
-      </header>
-    </div>
+    <button>
+      Generate
+    </button>
   );
 }
 
+export const App = () => {
+  return (
+    <div className='container'>
+      <div className='content'>
+        <h4>Messege</h4>
+        <VerseBut />
+      </div>
+    </div>
+  );
+}
 export default App;
