285d7441721a762216e621faeecc1703383dab38
jebcoding <johnbvillegas11@gmail.com>
2024-10-02 18:42:43 +0800

    hellojb

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-function VerseBut() {
-  return (
-    <button className='button-29'>
-      Generate
-    </button>
-  );
-}
+const verses = [
+  {
+    text: "For God so loved the world that he gave his only begotten Son, that whoever believes in Him should not perish but have everlasting life.",
+    reference: "John 3:16"
+  },
+  {
+    text: "The Lord is my shepherd; I shall not want.",
+    reference: "Psalm 23:1"
+  },
+  {
+    text: "I can do all things through Christ who strengthens me.",
+    reference: "Philippians 4:13"
+  }
+];
 
-export const App = () => {
+function BibleVerseGenerator() {
+  const [verse, setVerse] = useState(verses[Math.floor(Math.random() * verses.length)]);
   return (
     <div className='container'>
       <div className='content'>
@@ -18,13 +26,19 @@ export const App = () => {
           <img src='./assets/tm logo.png' id='tclogo'/>
         </h4>
             <div className='verse-display'>
-              <p><b>"</b><span id='versetext'>For God so love the world that he gave is only begotten son,
-                that who ever believes on him should not perish, But have eternal life</span><b>"</b></p>
-              <p id='verses'><span id='book'>JOHN</span> <span id='chapter'>3</span>:<span id='verse'>16</span></p>
+              <p><b>"</b><span id='versetext'>{verse.text}</span><b>"</b></p>
+              <p id='verses'>{verse.reference}</p>
             </div>
       </div>
-      <VerseBut />
+      <button className='button-29' onClick={() => setVerse(verses[Math.floor(Math.random() * verses.length)])}>
+      Generate
+    </button>
     </div>
+  );
+}
+export const App = () => {
+  return (
+    <div><BibleVerseGenerator/></div>  
   );
 }
 export default App;
